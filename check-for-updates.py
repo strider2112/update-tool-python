4b09a197ce716f73d208378d10dcd63ddb845a1c
Shane <strider2112@users.noreply.github.com>
2018-04-24 13:07:00 -0600

    Add files via upload

---
+++ check-for-updates.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os, sys, updatemodule
+
+# os.path.dirname(os.path.realpath()) will ignore symlinks
+programRoot = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+# configure the files here, these should point to the installation directory
+holderFile = programRoot + "/data/updates.txt"
+zeroUpdateFile = programRoot + "/data/zeroUpdates.txt"
+
+# put the apt-check path here
+aptCheckFile = "/usr/lib/update-notifier/apt-check"
+
+updatemodule.dependancies(aptCheckFile)
+updatemodule.checks(holderFile, zeroUpdateFile)
+updatemodule.zeroUpdates(zeroUpdateFile)
+updatemodule.update(aptCheckFile, holderFile, zeroUpdateFile)
