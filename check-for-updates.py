3528318b7093593c2424437d71cf8a8221e60ce2
Shane <strider2112@users.noreply.github.com>
2018-04-24 13:15:18 -0600

    Update check-for-updates.py
    Update to make the main function.

--- check-for-updates.py
+++ check-for-updates.py
@@ -2,6 +2,7 @@
 
 import os, sys, updatemodule
 
+# ToDo: Create a .conf file to hold this kind of data
 # os.path.dirname(os.path.realpath()) will ignore symlinks
 programRoot = os.path.dirname(os.path.realpath(sys.argv[0]))
 
@@ -12,7 +13,11 @@ zeroUpdateFile = programRoot + "/data/zeroUpdates.txt"
 # put the apt-check path here
 aptCheckFile = "/usr/lib/update-notifier/apt-check"
 
-updatemodule.dependancies(aptCheckFile)
-updatemodule.checks(holderFile, zeroUpdateFile)
-updatemodule.zeroUpdates(zeroUpdateFile)
-updatemodule.update(aptCheckFile, holderFile, zeroUpdateFile)
+def main():
+  updatemodule.dependancies(aptCheckFile)
+  updatemodule.checks(holderFile, zeroUpdateFile)
+  updatemodule.zeroUpdates(zeroUpdateFile)
+  updatemodule.update(aptCheckFile, holderFile, zeroUpdateFile)
+
+if __name__ == "__main__":
+  main()
