8807289d108ccd09df4bddf788215e0d196f18a4
Shane <strider2112@users.noreply.github.com>
2018-06-01 09:10:45 -0600

    Bug fix: update not completed
    Fixed a bug where the script would not run check-for-updates

--- update_tool_python/updater/updater_main.py
+++ update_tool_python/updater/updater_main.py
@@ -4,7 +4,7 @@ from ..common.updatemodule import query, updateFunction
 
 def main(DATADIR, DIR):
 	updateFile = DATADIR + "/updates.txt"
-	updateChecker = DIR + "check_for_updates.py"
+	updateChecker = "check-for-updates"
 
 	with open(updateFile) as uf:
 		lines = [line.rstrip('\n') for line in uf]
@@ -17,6 +17,10 @@ def main(DATADIR, DIR):
 	if not query("\nContinue with \033[01;32msudo apt upgrade\033[0m?","yes"):
 		exit()
 	updateFunction(2)
+	try:
+		os.system(updateChecker)
+	except:
+		print('An error occurred running \033[01;32mcheck-for-updates\033[0m')
 	if not query("\nUpdate complete, there may be leftover files, do you want to run autoremove?"):
 		exit()
 	updateFunction(3)
